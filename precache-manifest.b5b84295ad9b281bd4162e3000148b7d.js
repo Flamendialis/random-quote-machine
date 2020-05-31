da8abc5dac2b9d1a348dcb60e039eae9df70d951
Flamen_Dialis <menendezrfelixj@gmail.com>
2020-05-30 22:34:30 -0300

    Updates

---
+++ precache-manifest.b5b84295ad9b281bd4162e3000148b7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca6a9a3c5733fbee8d391874081e242",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "2e33eed9258ada3f4a8b",
+    "url": "/random-quote-machine/static/css/main.e6496eaf.chunk.css"
+  },
+  {
+    "revision": "7b9c1bbe1fed5253301c",
+    "url": "/random-quote-machine/static/js/2.13a60011.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/random-quote-machine/static/js/2.13a60011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e33eed9258ada3f4a8b",
+    "url": "/random-quote-machine/static/js/main.32a465d8.chunk.js"
+  },
+  {
+    "revision": "2cec414998c69e0a692f",
+    "url": "/random-quote-machine/static/js/runtime-main.5e7c1c70.js"
+  }
+]);
